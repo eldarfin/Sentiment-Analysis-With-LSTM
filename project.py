2b60b51354a40e4377af052fd4804e6282aef362
eldarfin <egeberkay12@gmail.com>
2018-12-20 00:11:01 +0300

    project finished, only tests remain

--- project.py
+++ project.py
@@ -1,68 +1,56 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
-from gensim.models import Word2Vec, KeyedVectors
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM
-from keras.layers.embeddings import Embedding
-from keras.initializers import Constant
+import numpy as np 
+from keras.preprocessing.text import one_hot
+from keras_preprocessing import sequence
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense
 
-
-
+### Preprocess data ###
 negative = open('negative10kmod.txt','r').readlines()
 positive = open('positive10kmod.txt','r').readlines()
 
+temp = []
+for line in negative:
+    temp.append(line.strip())
+negative = temp.copy()
 
-train_positive = positive[:7000]
-test_positive = positive[7000:]
-train_negative = negative[:7000]
-test_negative = negative[7000:]
+temp = []
+for line in positive:
+    temp.append(line.strip())
+positive = temp.copy()
 
-x_train = train_positive + train_negative
-y_train = [1] * 7000 + [0] * 7000
-x_test = test_positive + test_negative
+positive = [line.lower() for line in positive]
+negative = [line.lower() for line in negative]
+
+### Create word vectors ###
+negative_vocab = len(sorted(set(negative)))
+positive_vocab = len(sorted(set(positive)))
+vocab = len(sorted(set(positive+negative)))
+
+encoded_negative = [one_hot(line, negative_vocab) for line in negative]
+encoded_positive = [one_hot(line, positive_vocab) for line in positive]
+
+x_train = encoded_positive[:6000] + encoded_negative[:6000]
+x_validation = encoded_positive[6000:7000] + encoded_negative[6000:7000]
+x_test = encoded_positive[7000:] + encoded_negative[7000:]
+y_train = [1] * 6000 + [0] * 6000
+y_validation = [1] * 1000 + [0] * 1000
 y_test = [1] * 2926 + [0] * 2704
 
-# Preprocess training set
-temp = []
-for line in x_train:
-    tokens = word_tokenize(line)
-    tokens = [w.lower() for w in tokens]
-    table = str.maketrans('', '', string.punctuation)
-    stripped = [w.translate(table) for w in tokens]
-    words = [words for word in stripped if word.isalpha()]
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
-    temp.append(words)
+max_review_length = 350
+x_train = sequence.pad_sequences(x_train, maxlen=max_review_length)
+x_test = sequence.pad_sequences(x_test, maxlen=max_review_length)
 
-x_train = temp
-
-# Preprocess test set
-temp = []
-for line in x_test:
-    tokens = word_tokenize(line)
-    tokens = [w.lower() for w in tokens]
-    table = str.maketrans('', '', string.punctuation)
-    stripped = [w.translate(table) for w in tokens]
-    words = [words for word in stripped if word.isalpha()]
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
-    temp.append(words)
-
-x_test = temp
-
-# Train word2vec model
-model_train = Word2Vec(sentences=x_train, size=100, window=5, workers=4, min_count=1)
-vector = model_train.wv
-keras_embedding_layer = vector.get_keras_embedding(train_embeddings=True)
-
+### Build the model ###
+embedding_vector_length = 32
 model = Sequential()
-model.add(keras_embedding_layer)
-model.add(LSTM(units=100, dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
+model.add(Embedding(vocab, embedding_vector_length, input_length=max_review_length))
+model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
-
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size=128, epochs=25, validation_data=(x_test, y_test), verbose=2)
-model.save('keras_lstm.model')
 
+### Model training ###
+model.fit(x_train, y_train, validation_data=(x_test,  y_test), nb_epoch=3, batch_size=64)
 
+### Model evaluation ###
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
